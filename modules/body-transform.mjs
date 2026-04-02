c6476ed36a1f86c62afe1b2f2b161b184249fc66
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-02 18:26:02 -0500

    style: js-beautify formatting (modules/body-transform.mjs)

--- modules/body-transform.mjs
+++ modules/body-transform.mjs
@@ -1,11 +1,12 @@
-import { addLangs } from './langs.mjs';
+import {
+  addLangs
+} from './langs.mjs';
 await import('./preload-css.js');
 let cssmods = await (await fetch('https://files-servleteer.vercel.app/lenguapedia/default/mods.css')).text();
 
-
 export default function transformBody(resBody, ct, hostList, hostProxy, xlangs, bkcolor) {
   cssmods.replaceAll('cce9ff', bkcolor)
-  .replaceAll('rgba(255,255,255,0.69)', bkcolor);
+    .replaceAll('rgba(255,255,255,0.69)', bkcolor);
   const hostList_length = hostList.length;
   if (ct.includes('javascript')) {
     for (let i = 0; i < hostList_length; i++) {
@@ -13,15 +14,15 @@ export default function transformBody(resBody, ct, hostList, hostProxy, xlangs, 
     }
   }
 
-  let bodyTagHead = resBody.match(/<body[^>]*>/)?.[0]||'<body>';
+  let bodyTagHead = resBody.match(/<body[^>]*>/)?.[0] || '<body>';
 
   resBody = resBody.replace(/<body[^>]*>/,
     bodyTagHead +
     `<lenguapedia` +
     `   host-list="` + btoa(JSON.stringify(hostList)) +
     `"  host-Proxy="` + btoa(hostProxy) +
-    `" xlangs="` + xlangs + `"></lenguapedia>`
-    + preloadCSS +
+    `" xlangs="` + xlangs + `"></lenguapedia>` +
+    preloadCSS +
     `<script src="/static/link-resolver.v.js?4"></script>
       <script src="/static/inject-langs.js?2" onerror="(function(){let s=document.createElement('script');s.src='https://lenguapedia.org/static/inject-langs.js';document.body.appendChild(s);})();"></script>
       <link rel="stylesheet" href="/static/mods.css"></link>
@@ -29,8 +30,8 @@ export default function transformBody(resBody, ct, hostList, hostProxy, xlangs, 
     `<lenguapedia` +
     `   host-list="` + btoa(JSON.stringify(hostList)) +
     `"  host-Proxy="` + btoa(hostProxy) +
-    `" xlangs="` + xlangs + `"></lenguapedia>`
-    + preloadCSS +
+    `" xlangs="` + xlangs + `"></lenguapedia>` +
+    preloadCSS +
     `<script src="/static/link-resolver.v.js?4"></script>
       <script src="/static/inject-langs.js?2"></script>
       <link rel="stylesheet" href="https://files-servleteer.vercel.app/lenguapedia/default/mods.css"></link>
